7d7676e767252ecdfd12bb990b8abdd6242b693e
sarah beirigø <105953328+beirig0@users.noreply.github.com>
2023-08-19 20:14:26 -0300

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -2,19 +2,15 @@ using namespace std;
 #include <iostream>
 #include "free_cell.hpp"
 
-<<<<<<< Updated upstream
 int main()
 {
-=======
-int main(){
-
-Stack Pilha1;
-cout << Pilha1.Full() << endl;
-cout << Pilha1.Empty();
-
-Pilha1.Push(10);
->>>>>>> Stashed changes
 
     Stack Pilha1;
-    cout<<Pilha1.Empty();
+    cout << Pilha1.Full() << endl;
+    cout << Pilha1.Empty();
+
+    Pilha1.Push(10);
+
+    Stack Pilha1;
+    cout << Pilha1.Empty();
 }
