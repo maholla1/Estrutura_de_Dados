b17cac525cedf5fb1db18ec2af20ab6c58d32d3e
sarah beirigø <105953328+beirig0@users.noreply.github.com>
2023-08-19 19:12:39 -0300

    mudança implementação variavel top
    top -> entry[top]

--- driver.cpp
+++ driver.cpp
@@ -2,14 +2,9 @@ using namespace std;
 #include <iostream>
 #include "free_cell.hpp"
 
-int main(){
+int main()
+{
 
-Stack Pilha1;
-// cout << Pilha1.Full() << endl;
-// cout << Pilha1.Empty();
-
-
-
-Pilha1.Push(10);
-
+    Stack Pilha1;
+    cout<<Pilha1.Empty();
 }
--- free_cell.cpp
+++ free_cell.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 
-
-Stack::Stack(){
-    top=0;
+Stack::Stack()
+{
+    Entry[top] = 0;
 }
 
 // Stack::~Stack(){
@@ -12,42 +12,47 @@ Stack::Stack(){
 //     cout << "Chamado destrutor da pilha" << endl;
 // }
 
-bool Stack::Full(){
-    return top = MaxStack;
-
+bool Stack::Full()
+{
+    return Entry[top]= MaxStack;
 }
 
-bool Stack::Empty(){
-    return top==0;
-    cout << "A pilha esta vazia" << endl;
+bool Stack::Empty()
+{
+    return top == 0;
 }
 
-void Stack::Push(int x){
-    
-    if(Full()){
+void Stack::Push(int x)
+{
+ 
+    if (Full())
+    {
         cout << "A pilha esta cheia! Elemento " << x << " nao inserido." << endl;
-        return; //encerra apenas a função
+        return; // encerra apenas a função
     }
     top++;
     Entry[top] = x;
-    
 }
 
-void Stack::Pop(int &x){
+void Stack::Pop(int &x)
+{
 
-    if(Empty()){
+    if (Empty())
+    {
         cout << "A pilha esta vazia! Elemento nao pode ser removido." << endl;
         return;
-    } 
+    }
     x = Entry[top];
     top--;
 }
 
-void Stack::Top(int &x){
+void Stack::Top(int &x)
+{
 
-    if(Empty()){
+    if (Empty())
+    {
         cout << "A pilha esta vazia! Nao ha elemento no topo." << endl;
         return;
-    } 
+    }
     x = Entry[top];
 }
